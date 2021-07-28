cc5af80b30a8086890c393febf2b3028b2c333d3
Wan Arif <54633167+0ne4rif@users.noreply.github.com>
2021-07-28 18:28:00 +0800

    Add files via upload

---
+++ src/app/shared/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { TokenStorageService } from '../services/token-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    public auth: TokenStorageService,
+    public router: Router
+  ) { }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | boolean {
+    if (this.auth.isLoggedIn !== true) {
+      window.alert("Please login to proceed!");
+      this.router.navigate(['']);
+    }
+    return true;
+  }
+  
+}
